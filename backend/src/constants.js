6ff27d6a820808dda5c53dcd9c2dd01fbc13ed62
Urvit Gehlot <urvitgehlotug@gmail.com>
2026-09-13 19:49:46 +0530

    feat: add api service with token refresh interceptor and backend constants

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -2,7 +2,8 @@ export const DB_NAME = "ug_chat"
 
 export const httpsOptions = {
     httpOnly: true,
-    sameSite: "lax",
+    // sameSite: "lax",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production" ? true : false,
     maxAge: (process.env.COOKIE_MAX_AGE_DAYS || 1) * 24 * 60 * 60 * 1000,
 }
